9efec608dae0426938df150f6f573aa52a785ea3
NotSerge <36100688+Not-Serge@users.noreply.github.com>
2024-02-27 15:45:34 +0800

    Update add_merchandise.php

--- add_merchandise.php
+++ add_merchandise.php
@@ -16,20 +16,20 @@ if(isset($_POST['publish'])){
    $title = $_POST['title'];
    $content = $_POST['content'];
 
-   $select_image = $conn->prepare("SELECT * FROM `merchandise` WHERE image = ? AND admin_id = ?");
-   $select_image->execute([$image, $admin_id]);
-
    if(isset($_FILES['image']['name'])){
       $image = $_FILES['image']['name'];
       $image = filter_var($image, FILTER_SANITIZE_STRING);
       $image_size = $_FILES['image']['size'];
       $image_tmp_name = $_FILES['image']['tmp_name'];
-      $image_folder = '/storage/uploads/'.$image;
+      $image_folder = '../storage/uploads/'.$image;
+
+      $select_image = $conn->prepare("SELECT * FROM `merchandise` WHERE image = ? AND admin_id = ?");
+      $select_image->execute([$image, $admin_id]);
 
       if($select_image->rowCount() > 0 AND $image != ''){
-         $message[] = 'please rename your image!';
+         $message[] = 'Please rename your image!';
       }elseif($image_size > 2000000){
-         $message[] = 'images size is too large!';
+         $message[] = 'Image size is too large!';
       }else{
          move_uploaded_file($image_tmp_name, $image_folder);
       }
@@ -40,7 +40,7 @@ if(isset($_POST['publish'])){
    if(empty($message)){
       $insert_merchandise = $conn->prepare("INSERT INTO `merchandise`(admin_id, admin_name, title, content, image, status) VALUES(?,?,?,?,?,?)");
       $insert_merchandise->execute([$admin_id, $admin_name, $title, $content, $image, $status]);
-      $message[] = 'merchandise published!';
+      $message[] = 'Merchandise published!';
    }
 }
 
@@ -55,16 +55,16 @@ if(isset($_POST['draft'])){
    $image = filter_var($image, FILTER_SANITIZE_STRING);
    $image_size = $_FILES['image']['size'];
    $image_tmp_name = $_FILES['image']['tmp_name'];
-   $image_folder = '/storage/uploads/'.$image;
+   $image_folder = '../storage/uploads/'.$image;
 
    $select_image = $conn->prepare("SELECT * FROM `merchandise` WHERE image = ? AND admin_id = ?");
    $select_image->execute([$image, $admin_id]);
 
    if(isset($image)){
       if($select_image->rowCount() > 0 AND $image != ''){
-         $message[] = 'image name repeated!';
+         $message[] = 'Image name repeated!';
       }elseif($image_size > 2000000){
-         $message[] = 'images size is too large!';
+         $message[] = 'Image size is too large!';
       }else{
          move_uploaded_file($image_tmp_name, $image_folder);
       }
@@ -73,11 +73,11 @@ if(isset($_POST['draft'])){
    }
 
    if($select_image->rowCount() > 0 AND $image != ''){
-      $message[] = 'please rename your image!';
+      $message[] = 'Please rename your image!';
    }else{
       $insert_merchandise = $conn->prepare("INSERT INTO `merchandise`(admin_id, admin_name, title, content, image, status) VALUES(?,?,?,?,?,?)");
       $insert_merchandise->execute([$admin_id, $admin_name, $title, $content, $image, $status]);
-      $message[] = 'draft saved!';
+      $message[] = 'Draft saved!';
    }
 }
 
@@ -90,7 +90,7 @@ error_reporting(E_ALL & ~E_DEPRECATED);
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>merchandise</title>
+   <title>Merchandise</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -107,25 +107,25 @@ error_reporting(E_ALL & ~E_DEPRECATED);
 <section class="post-editor">
 
    <br><br><br>
-   <h1 class="heading">add new merchandise</h1>
+   <h1 class="heading">Add New Merchandise</h1>
    <br><br><br>
 
    <form action="" method="post" enctype="multipart/form-data">
-      <input type="hidden" name="admin_name" value="<?= $fetch_profile['name']; ?>">
+      <input type="hidden" name="admin_name" value="<?= $admin_name; ?>">
       
-      <p>merchandise image</p>
+      <p>Merchandise Image</p>
       <input type="file" name="image" class="box" accept="image/jpg, image/jpeg, image/png, image/webp">
      
-      <p>merchandise title <span>*</span></p>
-      <input type="text" name="title" maxlength="100" required placeholder="add merchandise title" class="box">
+      <p>Merchandise Title <span>*</span></p>
+      <input type="text" name="title" maxlength="100" required placeholder="Add merchandise title" class="box">
       
-      <p>description <span>*</span></p>
-      <textarea name="content" class="box" required maxlength="10000" placeholder="write your description..." cols="30" rows="10"></textarea>
+      <p>Description <span>*</span></p>
+      <textarea name="content" class="box" required maxlength="10000" placeholder="Write your description..." cols="30" rows="10"></textarea>
 
       <br> <br><br>
       <div class="flex-btn">
-         <input type="submit" value="publish merchandise" name="publish" class="btn">
-		 <input type="submit" value="save draft" name="draft" class="option-btn">
+         <input type="submit" value="Publish Merchandise" name="publish" class="btn">
+		 <input type="submit" value="Save Draft" name="draft" class="option-btn">
       </div>
    </form>
 
