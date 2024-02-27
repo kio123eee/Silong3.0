47cdfa3a59783b1fb2e889953b2d74b139769ac2
kio123eee <89443091+kio123eee@users.noreply.github.com>
2024-02-27 11:36:53 +0800

    Update admin_posts.php

--- admin_posts.php
+++ admin_posts.php
@@ -17,7 +17,7 @@ if(isset($_POST['delete'])) {
 	$select_image->execute([$delete_id]);
 	$fetch_image = $select_image->fetch(PDO::FETCH_ASSOC);
 	if($fetch_image['image'] != ''){
-		unlink('/uploads/'.$fetch_image['image']);
+		unlink('../frontendPHP/'.$fetch_image['image']);
 	}
 	$delete_post = $conn->prepare("DELETE FROM `posts` WHERE id = ?");
 	$delete_post->execute([$delete_id]);
